00e2cade250de502bbd9a9ff7b5d46f386d466bf
KarenMkrtchyan <70455623+KarenMkrtchyan@users.noreply.github.com>
2023-06-15 11:15:05 -0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,24 +2,100 @@
 #include <Servo.h>
 #include <math.h>
 
-int Serv1 = 3;
-float a = 5.12;
-float b = 5.32;
-;
+int kneePin = 3;
+int hipPin = 5;
+
+int smooth;
+int prev =0;
+
+float a = 2.67;
+float b = 2.67;
+float c;
+
 double angle;
 
-Servo serv;
+Servo knee;
+Servo hip;
+
+int xPin = A0;
+int ypin = A1;
+
+int x;
+int y;
+
+int sample = 20;
+int minChange = 10;
+
+int prevX;
+int prevY;
+
+double kneeAngle(float a,float b, float c){
+
+  
+  double angle = sq(a)+sq(b)-sq(c);
+  angle = angle/(2*a*b);
+  angle = acos(angle)*180/PI;
+
+  return angle;
+}
+
+
+//Angle for hip given a=b
+double hipAngle(double c, double x){
+  double angle = atan(x/c);
+  angle = angle*180/PI;
+  return angle;
+}
+
 
 void setup() {
-  serv.attach(Serv1);
- 
+  knee.attach(kneePin);
+  hip.attach(hipPin);
+
+  pinMode(xPin,INPUT);
+  pinMode(ypin,INPUT);
+  knee.write(0);
+  hip.write(0);
+
+  Serial.begin(9600);
+
+
 }
 
+
 void loop() {
-  for(float x = 4; x<=8; x+=.1){
-  angle = acos((sq(a)+sq(b)-sq(x))/(2*a*b));
-  angle =  angle * 180 / 3.14;
-  serv.write(angle);
+  x = analogRead(xPin);
+  y= analogRead(ypin);
+  // Serial.print("X: ");
+  // Serial.print(x);
+  // Serial.print("  Y: ");
+  //   Serial.println(y);
+
+    for(int i =0; i<=sample; i++){
+         x += analogRead(xPin);
+         y += analogRead(ypin);
+
+    }
+    x = x/sample;
+    y = y/sample;
+
+    if(abs(x-prevX)>minChange){       
+        prevX =x;
+    }
+if(abs(y-prevY)>minChange){     
+      prevY=y;
+      c = prevY*.0048;
+    }
+
+    Serial.println(kneeAngle(a,b,c));
+    
+
+    knee.write(kneeAngle(a,b,c));
+    hip.write(hipAngle(c,prevX*.0048));
+    
+    
   delay(100);
-  }
+
+   
 }
+
